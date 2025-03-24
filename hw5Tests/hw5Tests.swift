2bbdc5b5be7874d86b26fe148b30718a09220a1d
Showerxxl <mark1n.igor@yandex.ru>
2025-03-24 17:23:11 +0300

    d

---
+++ hw5Tests/hw5Tests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import hw5
+
+final class hw5Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+    func testPerformanceExample() throws {
+
+        self.measure {
+
+        }
+    }
+
+}
